a13f6ea39315d48e426af6fb6ec02250ef84ee96
Arnav Ojha <arnav.ojha@lodhaworldschool.com>
2024-07-10 19:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,35 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def calculation(money, units_of_energy):
-    price_per_unit = round(float(money) / float(units_of_energy), 2)
-    price_per_solar = round((0.20 * float(units_of_energy)), 2)  # Assuming you want a float result
-    return price_per_unit, price_per_solar
+tradition_energy_price = 0.15
+solar_energy_price = 0.06
+solar_panel_price = 600
+
+def calculation(money):
+    units_of_energy = round(float(money) / tradition_energy_price, 2)
+    price_using_solar = round(units_of_energy * solar_energy_price, 2)
+    saving = float(money) - price_using_solar
+    return price_using_solar, saving
+
+def solar_panel(savings):
+    months = round(solar_panel_price / savings)
+    days = round(months * 30)
+
+    if months == 0 and days == 0:
+        return 1, 30
+
+    return months, days
+
+def investment(savings, planning_years, months_to_pay_for_panel):
+    totalMonths = planning_years * 12
+    monthsEarning = totalMonths - months_to_pay_for_panel
+    totalMonths_Earning = monthsEarning * savings
+    earning_without_maintainance = monthsEarning * savings
+    earning_with_maintainance = earning_without_maintainance - 1000
+
+    total_saving = savings * totalMonths
+
+    return totalMonths, monthsEarning, totalMonths_Earning, earning_without_maintainance, earning_with_maintainance, total_saving
 
 @app.route('/')
 def index():
@@ -13,10 +38,47 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    bill = request.form.get('bill')
-    energy = request.form.get('energy')
-    price_per_unit, price_per_solar = calculation(bill, energy)
-    return render_template('Web-Page.html', bill=bill, energy=energy, price_per_solar=price_per_solar, price_per_unit=price_per_unit)
+    try:
+        bill = request.form.get('bill')
+        
+        if bill:
+            bill = float(bill)
+        else:
+            return render_template('Web-Page.html')
+
+        price_using_solar, savings = calculation(bill)
+        months, days = solar_panel(savings)
+
+        return render_template('Web-Page.html', bill=bill, price_using_solar=price_using_solar, savings=savings, months=months, days=days)
+        
+    except ValueError:
+        return render_template('Web-Page.html')
+
+@app.route('/result_investment', methods=['POST'])
+def result_investment():
+    try:
+        bill = request.form.get('bill')
+        planning_years = request.form.get('planning_years')
+
+        if bill:
+            bill = float(bill)
+        else:
+            return render_template('Web-Page.html')
+
+        if planning_years:
+            planning_years = int(planning_years)
+
+        price_using_solar, savings = calculation(bill)
+        months, days = solar_panel(savings)
+
+        totalMonths, monthsEarning, totalMonths_Earning, earning_without_maintainance, earning_with_maintainance, total_saving = investment(savings, planning_years, months)
+        totalMonths, monthsEarning, totalMonths_Earning, earning_without_maintainance, earning_with_maintainance, total_saving = round(totalMonths), round(monthsEarning), round(totalMonths_Earning), round(earning_without_maintainance), round(earning_without_maintainance), round(total_saving)
+
+
+        return render_template('Web-Page.html', bill=bill, price_using_solar=price_using_solar, savings=savings, months=months, days=days, planning_years=planning_years, totalMonths=totalMonths, monthsEarning=monthsEarning, totalMonths_Earning=totalMonths_Earning, earning_without_maintainance=earning_without_maintainance, earning_with_maintainance=earning_with_maintainance, total_saving = total_saving)
+        
+    except ValueError:
+        return render_template('Web-Page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
